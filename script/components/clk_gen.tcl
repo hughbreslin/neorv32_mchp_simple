714313278765288ae6d2e37e82bcb956214c98cd
Hugh Breslin - M31864 <Hugh.Breslin@microchip.com>
2025-07-17 15:45:56 +0200

    [design] fix clock generation
    The 100MHz clock and 150MHz clock generation was mixed up - now properly
    generating those clocks

--- script/components/clk_gen.tcl
+++ script/components/clk_gen.tcl
@@ -36,12 +36,12 @@ sd_instantiate_component -sd_name ${sd_name} -component_name {PF_INIT_MONITOR_C0
 
 
 # Add RESET_CLK_150MHz instance
-sd_instantiate_component -sd_name ${sd_name} -component_name {RESET_WRAPPER} -instance_name {RESET_CLK_150MHz}
+sd_instantiate_component -sd_name ${sd_name} -component_name {RESET_WRAPPER} -instance_name {RESET_CLK_100MHz}
 
 
 
 # Add RESET_CLK_150MHz_0 instance
-sd_instantiate_component -sd_name ${sd_name} -component_name {RESET_WRAPPER} -instance_name {RESET_CLK_150MHz_0}
+sd_instantiate_component -sd_name ${sd_name} -component_name {RESET_WRAPPER} -instance_name {RESET_CLK_150MHz}
 
 
 
@@ -58,17 +58,17 @@ sd_instantiate_component -sd_name ${sd_name} -component_name {RESET_WRAPPER} -in
 # Add scalar net connections
 sd_connect_pins -sd_name ${sd_name} -pin_names {"CLKINT_0:A" "ref_clk_50MHz" }
 sd_connect_pins -sd_name ${sd_name} -pin_names {"CLKINT_0:Y" "PF_CCC_C0_0:REF_CLK_0" }
-sd_connect_pins -sd_name ${sd_name} -pin_names {"EXT_RST_N" "RESET_CLK_150MHz:EXT_RST_N" "RESET_CLK_150MHz_0:EXT_RST_N" "RESET_CLK_200MHz:EXT_RST_N" "RESET_CLK_250MHz:EXT_RST_N" }
+sd_connect_pins -sd_name ${sd_name} -pin_names {"EXT_RST_N" "RESET_CLK_100MHz:EXT_RST_N" "RESET_CLK_150MHz:EXT_RST_N" "RESET_CLK_200MHz:EXT_RST_N" "RESET_CLK_250MHz:EXT_RST_N" }
 sd_connect_pins -sd_name ${sd_name} -pin_names {"PF_CCC_C0_0:OUT0_FABCLK_0" "RESET_CLK_250MHz:CLK" "clk_250MHz" }
 sd_connect_pins -sd_name ${sd_name} -pin_names {"PF_CCC_C0_0:OUT1_FABCLK_0" "RESET_CLK_200MHz:CLK" "clk_200MHz" }
-sd_connect_pins -sd_name ${sd_name} -pin_names {"PF_CCC_C0_0:OUT2_FABCLK_0" "RESET_CLK_150MHz:CLK" "clk_100MHz" }
-sd_connect_pins -sd_name ${sd_name} -pin_names {"PF_CCC_C0_0:OUT3_FABCLK_0" "RESET_CLK_150MHz_0:CLK" "clk_150MHz" }
-sd_connect_pins -sd_name ${sd_name} -pin_names {"PF_CCC_C0_0:PLL_LOCK_0" "RESET_CLK_150MHz:PLL_LOCK" "RESET_CLK_150MHz_0:PLL_LOCK" "RESET_CLK_200MHz:PLL_LOCK" "RESET_CLK_250MHz:PLL_LOCK" }
+sd_connect_pins -sd_name ${sd_name} -pin_names {"PF_CCC_C0_0:OUT2_FABCLK_0" "RESET_CLK_150MHz:CLK" "clk_150MHz" }
+sd_connect_pins -sd_name ${sd_name} -pin_names {"PF_CCC_C0_0:OUT3_FABCLK_0" "RESET_CLK_100MHz:CLK" "clk_100MHz" }
+sd_connect_pins -sd_name ${sd_name} -pin_names {"PF_CCC_C0_0:PLL_LOCK_0" "RESET_CLK_100MHz:PLL_LOCK" "RESET_CLK_150MHz:PLL_LOCK" "RESET_CLK_200MHz:PLL_LOCK" "RESET_CLK_250MHz:PLL_LOCK" }
 sd_connect_pins -sd_name ${sd_name} -pin_names {"PF_CCC_C0_0:PLL_POWERDOWN_N_0" "RESET_CLK_250MHz:POWERDOWN_B" }
-sd_connect_pins -sd_name ${sd_name} -pin_names {"PF_INIT_MONITOR_C0_0:DEVICE_INIT_DONE" "RESET_CLK_150MHz:INIT_DONE" "RESET_CLK_150MHz_0:INIT_DONE" "RESET_CLK_200MHz:INIT_DONE" "RESET_CLK_250MHz:INIT_DONE" }
-sd_connect_pins -sd_name ${sd_name} -pin_names {"PF_INIT_MONITOR_C0_0:FABRIC_POR_N" "RESET_CLK_150MHz:FPGA_POR_N" "RESET_CLK_150MHz_0:FPGA_POR_N" "RESET_CLK_200MHz:FPGA_POR_N" "RESET_CLK_250MHz:FPGA_POR_N" }
+sd_connect_pins -sd_name ${sd_name} -pin_names {"PF_INIT_MONITOR_C0_0:DEVICE_INIT_DONE" "RESET_CLK_100MHz:INIT_DONE" "RESET_CLK_150MHz:INIT_DONE" "RESET_CLK_200MHz:INIT_DONE" "RESET_CLK_250MHz:INIT_DONE" }
+sd_connect_pins -sd_name ${sd_name} -pin_names {"PF_INIT_MONITOR_C0_0:FABRIC_POR_N" "RESET_CLK_100MHz:FPGA_POR_N" "RESET_CLK_150MHz:FPGA_POR_N" "RESET_CLK_200MHz:FPGA_POR_N" "RESET_CLK_250MHz:FPGA_POR_N" }
+sd_connect_pins -sd_name ${sd_name} -pin_names {"RESET_CLK_100MHz:FABRIC_RESET_N" "resetn_clk_100MHz" }
 sd_connect_pins -sd_name ${sd_name} -pin_names {"RESET_CLK_150MHz:FABRIC_RESET_N" "resetn_clk_150MHz" }
-sd_connect_pins -sd_name ${sd_name} -pin_names {"RESET_CLK_150MHz_0:FABRIC_RESET_N" "resetn_clk_100MHz" }
 sd_connect_pins -sd_name ${sd_name} -pin_names {"RESET_CLK_200MHz:FABRIC_RESET_N" "resetn_clk_200MHz" }
 sd_connect_pins -sd_name ${sd_name} -pin_names {"RESET_CLK_250MHz:FABRIC_RESET_N" "resetn_clk_250MHz" }
 
